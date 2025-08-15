b8a6f25ec80ffbf188df4b672d260d94a8d2645b
Asutosh Mishra <46391557+asumishra1698@users.noreply.github.com>
2025-08-15 22:57:48 +0530

    Delete webadmin directory

--- webadmin/src/redux/reducers/rootReducers.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import authReducers from "./authReducers";
-
-const rootReducer = combineReducers({
-  auth: authReducers,
-});
-
-export default rootReducer;
-export type RootState = ReturnType<typeof rootReducer>;
